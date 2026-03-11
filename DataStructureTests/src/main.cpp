22af8f95fb422247688feff138a05ae2926efe84
Kidus Workneh <kiwo9430@login12.chn.perlmutter.nersc.gov>
2026-03-10 20:07:31 -0700

    fixes initialization bug in bst

--- DataStructureTests/src/main.cpp
+++ DataStructureTests/src/main.cpp
@@ -82,13 +82,18 @@ void print_function(int duration, int64_t ops0, int64_t ops1, int64_t totalOps){
 
 
 void main_BST_test(int duration, int64_t num_DS, int num_threads, int crossover, int keyspace){
-	//Initialization
-	init_thread0 = new thread_numa<NODE_ZERO>(numa_BST_init, DS_config, num_DS/2, keyspace, 0,crossover);
-	init_thread1 = new thread_numa<MAX_NODE>(numa_BST_init, DS_config, num_DS/2, keyspace, 1,crossover);
-
-    if(init_thread0) init_thread0->join();
-    if(init_thread1) init_thread1->join();
-
+	if(thread_config == "numa"){
+		init_thread0 = new thread_numa<NODE_ZERO>(numa_BST_init, DS_config, num_DS/2, keyspace, 0,crossover);
+		init_thread1 = new thread_numa<MAX_NODE>(numa_BST_init, DS_config, num_DS/2, keyspace, 1,crossover);
+		if(init_thread0) init_thread0->join();
+    	if(init_thread1) init_thread1->join();
+	}
+	else if(thread_config == "regular"){
+		init_thread_regular0 = new std::thread(numa_BST_init, DS_config, num_DS/2, keyspace, 0,crossover);
+		init_thread_regular1 = new std::thread(numa_BST_init, DS_config, num_DS/2, keyspace, 1,crossover);
+		if(init_thread_regular0) init_thread_regular0->join();
+		if(init_thread_regular1) init_thread_regular1->join();
+	}
 	//Test
 	for(int i=0; i < num_threads/2; i++){
 		int node = 0;
